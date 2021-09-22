f169b57e3e66ca374592979a4b479f347d43b260
tabbykatz <tomelay@gmail.com>
2021-09-22 14:40:24 -0700

    add apiClient

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -1,29 +1,15 @@
 export const getContacts = () => _get("/api/contacts");
 
-export const addContact = (contact) => _post("/api/contacts", contact);
+export const addContact = (contact) => _add("/api/contacts", contact, "POST");
 
 export const updateContact = (contact, id) =>
-  _put(`/api/contacts/${id}`, contact);
+  _add(`/api/contacts/${id}`, contact, "PUT");
 
 const _get = async (url) => (await fetch(url)).json();
 
-const _put = async (url, body) => {
-  const res = await fetch(url, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
-  });
-  let result;
-  try {
-    result = await res.json();
-  } catch {}
-
-  return result;
-};
-
-const _post = async (url, body) => {
+const _add = async (url, body, method) => {
   const response = await fetch(url, {
-    method: "POST",
+    method,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
